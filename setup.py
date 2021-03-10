44074adbc3caaae5d0646a04f8b8eeabcbc080f3
Aleix Alcacer <aleixalcacer@gmail.com>
2021-03-10 22:52:18 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+
+CLASSIFIERS = """\
+Development Status :: 5 - Production/Stable
+Environment :: Console
+Intended Audience :: Developers
+License :: OSI Approved :: BSD License
+Natural Language :: English
+Operating System :: OS Independent
+Programming Language :: Python
+Topic :: Software Development :: Libraries :: Python Modules
+"""
+
+
+setup(
+    name="archetypes",
+    description="A scikit-learn compatible Python package for archetypal analysis.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    license="BSD (3-clause)",
+    version="0.0.1.dev0",
+    author="Aleix Alcacer",
+    author_email="aleixalcacer@gmail.com",
+    url="https://github.com/aleixalcacer/archetypes",
+    packages=["archetypes"],
+    install_requires=["scikit-learn", "numpy", "scipy"],
+    python_requires=">=3.7",
+    classifiers=list(filter(None, CLASSIFIERS.split("\n"))),
+)
