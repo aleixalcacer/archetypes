8367a74ce6b84d66db33ef99a7dc0a14c2318664
Aleix Alcacer <aleixalcacer@gmail.com>
2023-04-11 16:38:53 +0200

    Implement AA using pytorch

---
+++ archetypes/algorithms/torch/utils.py
@@ -0,0 +1,46 @@
+import torch
+
+
+def hardmax(tensor, dim):
+    id = [range(s) for s in tensor.shape]
+    id[dim] = torch.argmax(tensor, dim=dim)
+    tensor[...] = 0
+    tensor[id] = 1
+    return tensor
+
+
+def softmax(tensor, dim):
+    return torch.softmax(tensor, dim=dim)
+
+
+def normal_distance(X, maxoids):
+    return ((X - maxoids) ** 2).sum(axis=-1)
+
+
+def bernoulli_distance(X, maxoids):
+    return -(X * maxoids.log() + (1 - X) * (1 - maxoids).log()).sum(axis=-1)
+
+
+def poisson_distance(X, maxoids):
+    return -(X * maxoids.log() - maxoids).sum(axis=-1)
+
+
+def update_clusters_A(X, clusters, centroids, likelihood="normal"):
+    new_clusters = torch.zeros_like(clusters)
+
+    if likelihood == "bernoulli":
+        e = 1e-8
+        centroids[centroids == 0] = e
+        centroids[centroids == 1] = 1 - e
+
+    i = range(new_clusters.shape[0])
+    j = globals()[f"{likelihood}_distance"](X[:, None, :], centroids[None, :, :]).argmin(
+        axis=1
+    )  # Optimized
+    new_clusters[i, j] = 1
+
+    return new_clusters
+
+
+def update_clusters_D(X, clusters, maxoids, likelihood="normal"):
+    return update_clusters_A(X.T, clusters.T, maxoids.T, likelihood).T
--- bench/biaa.py
+++ bench/biaa.py
@@ -28,13 +28,13 @@ aa_kwargs = {
     "tol": 1e-8,
 }
 
-mod0 = arch.BiAA(**aa_kwargs, algorithm_init="random")
+mod0 = arch.AA(**aa_kwargs, algorithm_init="random")
 t0 = time()
 mod0.fit(data)
 t1 = time()
 print(f"mod0: {t1 - t0:.4f} s | RSS: {mod0.rss_:.2f}")
 
-mod1 = arch.BiAA(**aa_kwargs, algorithm_init="furthest_sum")
+mod1 = arch.AA(**aa_kwargs, algorithm_init="furthest_sum")
 t0 = time()
 mod1.fit(data)
 t1 = time()
