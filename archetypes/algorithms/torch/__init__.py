c86847ca5131560be4e03af0eb20c4df8ee7b754
Aleix Alcacer <aleixalcacer@gmail.com>
2023-11-21 10:01:26 +0100

    Fix imports

--- archetypes/algorithms/torch/__init__.py
+++ archetypes/algorithms/torch/__init__.py
@@ -1,6 +1,5 @@
 from .archetypes import AA
 from .biarchetypes import BiAA
 from .narchetypes import NAA
-from .narchetypes2 import NAA2
 
-__all__ = ["AA", "BiAA", "NAA", "NAA2"]
+__all__ = ["AA", "BiAA", "NAA"]
