9862153b47534548c04b2cc036e8a2be2a5fdd05
Aleix Alcacer Sales <aalcacer@uji.es>
2024-05-28 17:27:43 +0200

    Implement biaa with nnls and jax optimizers

--- archetypes/sklearn/_biaa.py
+++ archetypes/sklearn/_biaa.py
@@ -1,18 +1,24 @@
 import numpy as np
+import optax
+from custom_inherit import doc_inherit
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.utils import check_random_state
 from sklearn.utils.validation import check_is_fitted
 
-from ._inits import uniform
+from ..utils import arch_einsum
+from ._inits import aa_plus_plus, furthest_first, furthest_sum, uniform
+from ._optims import AAOptimizer, jax_biaa_optimizer, nnls_biaa_optimizer
+
+_ = uniform, furthest_sum, furthest_first, aa_plus_plus
 
 
-class AABase(BaseEstimator, TransformerMixin):
+class BiAABase(BaseEstimator, TransformerMixin):
     """
-    Archetype Analysis.
+    BiArchetype Analysis.
 
     Parameters
     ----------
-    n_archetypes : int, default=4
+    n_archetypes : tuple
         The number of archetypes to compute.
     max_iter : int, default=300
         Maximum number of iterations of the archetype analysis algorithm
@@ -35,14 +41,16 @@ class AABase(BaseEstimator, TransformerMixin):
     Attributes
     ----------
 
-    archetypes_ : np.ndarray of shape (n_archetypes, n_features)
+    archetypes_ : np.ndarray of shape (*n_archetypes)
         The computed archetypes.
-    similarity_degree_ : np.ndarray of shape (n_samples, n_archetypes)
-        The similarity degree of each sample to each archetype.
-    archetypes_similarity_degree_ : np.ndarray of shape (n_archetypes, n_samples)
-        The similarity degree of each archetype to each sample.
-    labels_ : np.ndarray of shape (n_samples,)
-        The label of each sample. It is the index of the closest archetype.
+    similarity_degree_ : list[np.ndarray] where each element
+    is of shape (n_samples, n_archetypes)
+        The similarity degree of each sample to each archetype in each dimension.
+    archetypes_similarity_degree_ : list[np.ndarray]where each element
+    is of shape (n_archetypes, n_samples)
+        The similarity degree of each archetype to each sample in each dimension.
+    labels_ : np.ndarray of shape list[(n_samples,)]
+        The label of each sample in each dimension. It is the index of the closest archetype.
     loss_ : list
         The loss at each iteration.
 
@@ -50,13 +58,15 @@ class AABase(BaseEstimator, TransformerMixin):
     ----------
 
     .. [1] "Archetypal Analysis" by Cutler, A. and Breiman, L. (1994).
-    .. [2] "Archetypal Analysis for machine learning and data mining"
-           by Morup, M. and Hansen, L. K. (2012).
+    .. [2] "Biarchetype Analysis: Simultaneous Learning of Observations
+           and Features Based on Extremes"
+           by Alcacer, A., Epifanio, I. and Gual-Arnau, X. (2024).
+
     """
 
     def __init__(
         self,
-        n_archetypes=4,
+        n_archetypes,
         max_iter=300,
         tol=1e-4,
         init="uniform",
@@ -75,16 +85,22 @@ class AABase(BaseEstimator, TransformerMixin):
         self.random_state = random_state
 
     def _check_data(self, X):
-        if X.shape[0] < self.n_archetypes:
+        if X.shape[0] < self.n_archetypes[0] or X.shape[1] < self.n_archetypes[1]:
             raise ValueError(
-                f"n_samples={X.shape[0]} should be >= n_archetypes={self.n_archetypes}."
+                f"n_archetypes should be less than the number of samples and features, "
+                f"got {self.n_archetypes} and {X.shape} instead."
             )
 
     def _check_parameters(self):
         # Check if n_archetypes is a positive integer
-        if not isinstance(self.n_archetypes, int):
+        if not isinstance(self.n_archetypes, tuple):
             raise TypeError
-        if self.n_archetypes <= 0:
+        # check len(n_archetypes) == 2
+        if len(self.n_archetypes) != 2:
+            raise ValueError(
+                f"n_archetypes should be a tuple of length 2, got {self.n_archetypes} instead."
+            )
+        if self.n_archetypes[0] <= 0 or self.n_archetypes[1] <= 0:
             raise ValueError(f"n_archetypes should be > 0, got {self.n_archetypes} instead.")
 
         # Check if max_iter is a positive integer
@@ -145,27 +161,43 @@ class AABase(BaseEstimator, TransformerMixin):
         self.random_state = check_random_state(self.random_state)
 
     def _init_B(self, X):
-        B = np.zeros((self.n_archetypes, X.shape[0]), dtype=np.float64)
+        B_0 = np.zeros((self.n_archetypes[0], X.shape[0]), dtype=np.float64)
 
         ind = self.init(
-            X, self.n_archetypes, random_state=self.random_state, kwargs=self.init_kwargs
+            X, self.n_archetypes[0], random_state=self.random_state, kwargs=self.init_kwargs
         )
 
         for i, j in enumerate(ind):
-            B[i, j] = 1
+            B_0[i, j] = 1
 
-        self.B_ = B
+        B_1 = np.zeros((self.n_archetypes[1], X.shape[1]), dtype=np.float64)
+
+        ind = self.init(
+            X.T, self.n_archetypes[1], random_state=self.random_state, kwargs=self.init_kwargs
+        )
+
+        for i, j in enumerate(ind):
+            B_1[i, j] = 1
+
+        self.B_ = [B_0, B_1]
 
     def _init_A(self, X):
         # TODO: Improve it?
-        A = np.zeros((X.shape[0], self.n_archetypes), dtype=np.float64)
+        A_0 = np.zeros((X.shape[0], self.n_archetypes[0]), dtype=np.float64)
 
-        ind = uniform(X, self.n_archetypes, random_state=self.random_state, kwargs=self.init_kwargs)
+        ind = self.random_state.choice(self.n_archetypes[0], X.shape[0], replace=True)
 
         for i, j in enumerate(ind):
-            A[j, i] = 1
+            A_0[i, j] = 1
 
-        self.A_ = A
+        A_1 = np.zeros((X.shape[1], self.n_archetypes[1]), dtype=np.float64)
+
+        ind = self.random_state.choice(self.n_archetypes[1], X.shape[1], replace=True)
+
+        for i, j in enumerate(ind):
+            A_1[i, j] = 1
+
+        self.A_ = [A_0, A_1]
 
     def _optim_A(self, X):
         pass
@@ -174,7 +206,10 @@ class AABase(BaseEstimator, TransformerMixin):
         pass
 
     def _loss(self, X):
-        return np.linalg.norm(X - self.A_ @ self.archetypes_) ** 2
+        X1 = X
+        archetypes = arch_einsum(self.B_, X1)
+        X2 = arch_einsum(self.A_, archetypes)
+        return np.linalg.norm(X1 - X2) ** 2
 
     def fit(self, X, y=None, **fit_params):
         """
@@ -206,7 +241,7 @@ class AABase(BaseEstimator, TransformerMixin):
         if self.save_init:
             self.B_init_ = self.B_.copy()
 
-        self.archetypes_ = self.B_ @ X
+        self.archetypes_ = arch_einsum(self.B_, X)
 
         rss = self._loss(X)
         self.loss_ = [rss]
@@ -220,7 +255,7 @@ class AABase(BaseEstimator, TransformerMixin):
             self._optim_A(X)
             self._optim_B(X)
 
-            self.archetypes_ = self.B_ @ X
+            self.archetypes_ = arch_einsum(self.B_, X)
 
             # Compute RSS
             rss = self._loss(X)
@@ -231,7 +266,7 @@ class AABase(BaseEstimator, TransformerMixin):
         # Set attributes
         self.similarity_degree_ = self.A_
         self.archetypes_similarity_degree_ = self.B_
-        self.labels_ = np.argmax(self.A_, axis=1)
+        self.labels_ = [np.argmax(A_i, axis=1) for A_i in self.A_]
 
         return self
 
@@ -277,3 +312,87 @@ class AABase(BaseEstimator, TransformerMixin):
             X transformed in the new space.
         """
         return self.fit(X, y, **fit_params).transform(X)
+
+
+@doc_inherit(parent=BiAABase, style="numpy_with_merge")
+class BiAA(BiAABase):
+    """
+    Parameters
+    ----------
+    method: str, default='nnls'
+        The optimization method to use for the archetypes and the coefficients.
+    method_kwargs : dict, default=None
+        Additional keyword arguments to pass to the optimization method. See TODO for more details.
+
+    """
+
+    def __init__(
+        self,
+        n_archetypes,
+        max_iter=300,
+        tol=1e-4,
+        init="uniform",
+        init_kwargs=None,
+        save_init=False,
+        verbose=False,
+        random_state=None,
+        method="nnls",
+        method_kwargs=None,
+    ):
+        super().__init__(
+            n_archetypes=n_archetypes,
+            max_iter=max_iter,
+            tol=tol,
+            init=init,
+            init_kwargs=init_kwargs,
+            save_init=save_init,
+            verbose=verbose,
+            random_state=random_state,
+        )
+
+        self.method = method
+        self.method_kwargs = method_kwargs
+
+        self._check_parameters_()
+
+    def _check_parameters_(self):
+        # Check if method is a valid optimization method
+        if self.method not in ["nnls", "jax"]:
+            raise ValueError(f"Invalid optimization method: {self.method}")
+
+        # Check if method_kwargs is a dictionary
+        if self.method_kwargs is None:
+            self.method_kwargs = {}
+        if self.method_kwargs is not None and not isinstance(self.method_kwargs, dict):
+            raise TypeError("method_kwargs should be a dictionary.")
+
+        # Check params for the optimization method
+        if self.method == "nnls":
+            self.method_c: AAOptimizer = nnls_biaa_optimizer  # dataclass
+            self.max_iter_optimizer = self.method_kwargs.get("max_iter_optimizer", None)
+            self.const = self.method_kwargs.get("const", 100.0)
+        elif self.method == "jax":
+            self.method_c: AAOptimizer = jax_biaa_optimizer
+            self.optimizer = self.method_kwargs.get("optimizer", "sgd")
+            # check optimizer is a optax optimizer
+            if not isinstance(self.optimizer, optax.GradientTransformation):
+                self.optimizer = eval(f"optax.{self.optimizer}")(
+                    **self.method_kwargs.get("optimizer_kwargs", {})
+                )
+
+        # TODO: Check if the parameters are valid for the optimization method
+
+    def _init_A(self, X):
+        return self.method_c.A_init(self, X)
+
+    def _init_B(self, X):
+        return self.method_c.B_init(self, X)
+
+    def _optim_B(self, X):
+        self.method_c.B_optimize(self, X)
+
+    def _optim_A(self, X):
+        self.method_c.A_optimize(self, X)
+
+    def fit(self, X, y=None, **fit_params):
+        return self.method_c.fit(self, X, y, **fit_params)
