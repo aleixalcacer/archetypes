9d70e2c4adf1c53121460c9cd7f9c76d99019042
Aleix Alcacer Sales <aalcacer@uji.es>
2025-06-05 17:20:27 +0200

    Implement KernelAA

--- archetypes/numpy/__init__.py
+++ archetypes/numpy/__init__.py
@@ -2,5 +2,6 @@ from ._aa import AA
 from ._ada import ADA
 from ._biaa import BiAA
 from ._fair_aa import FairAA
+from ._kernel_aa import KernelAA
 
-__all__ = ["AA", "FairAA", "ADA", "BiAA"]
+__all__ = ["AA", "FairAA", "ADA", "BiAA", "KernelAA"]
