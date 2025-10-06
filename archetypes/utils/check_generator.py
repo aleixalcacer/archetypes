f3b40293a82e437a86dd354d26978b7ab0e12cc7
Aleix Alcacer Sales <aalcacer@uji.es>
2025-10-06 10:42:04 +0200

    Remove torch and jax generator checks

--- archetypes/utils/check_generator.py
+++ archetypes/utils/check_generator.py
@@ -1,8 +1,6 @@
 import numbers
 
-import jax
 import numpy as np
-import torch
 
 
 def check_generator_numpy(seed=None):
@@ -27,48 +25,48 @@ def check_generator_numpy(seed=None):
     raise ValueError("%r cannot be used to seed a numpy.random.RandomState instance" % seed)
 
 
-def check_generator_jax(seed=None):
-    """Check seed and return a key for random number generation.
+# def check_generator_jax(seed=None):
+#     """Check seed and return a key for random number generation.
 
-    Parameters
-    ----------
-    seed : int or None
-        The generator to check. If None, 0 is used.
+#     Parameters
+#     ----------
+#     seed : int or None
+#         The generator to check. If None, 0 is used.
 
-    Returns
-    -------
-    key : KeyArray
-        The key to use for random number generation.
-    """
-    if seed is None:
-        return jax.random.key(0)
-    if isinstance(seed, int):
-        return jax.random.key(seed)
-    raise ValueError("%r cannot be used to seed a jax.random.KeyArray instance" % seed)
+#     Returns
+#     -------
+#     key : KeyArray
+#         The key to use for random number generation.
+#     """
+#     if seed is None:
+#         return jax.random.key(0)
+#     if isinstance(seed, int):
+#         return jax.random.key(seed)
+#     raise ValueError("%r cannot be used to seed a jax.random.KeyArray instance" % seed)
 
 
-def check_generator_torch(generator=None, device=None):
-    """Check if generator is an int, a Generator or None and return a Generator.
+# def check_generator_torch(generator=None, device=None):
+#     """Check if generator is an int, a Generator or None and return a Generator.
 
-    Parameters
-    ----------
-    generator : int, Generator or None
-        The generator to check. If None, the default generator is used.
-    device : str or torch.device
-        The device to use for the generator. If None, the default device is used.
+#     Parameters
+#     ----------
+#     generator : int, Generator or None
+#         The generator to check. If None, the default generator is used.
+#     device : str or torch.device
+#         The device to use for the generator. If None, the default device is used.
 
-    Returns
-    -------
-    generator : Generator
-        The checked generator.
-    """
+#     Returns
+#     -------
+#     generator : Generator
+#         The checked generator.
+#     """
 
-    if device is None:
-        device = "cpu"
-    if generator is None:
-        return torch.Generator(device=device)
-    if isinstance(generator, numbers.Integral):
-        return torch.Generator(device=device).manual_seed(generator)
-    if isinstance(generator, torch.Generator):
-        return generator
-    raise ValueError("%r cannot be used to seed a torch.Generator instance" % generator)
+#     if device is None:
+#         device = "cpu"
+#     if generator is None:
+#         return torch.Generator(device=device)
+#     if isinstance(generator, numbers.Integral):
+#         return torch.Generator(device=device).manual_seed(generator)
+#     if isinstance(generator, torch.Generator):
+#         return generator
+#     raise ValueError("%r cannot be used to seed a torch.Generator instance" % generator)
